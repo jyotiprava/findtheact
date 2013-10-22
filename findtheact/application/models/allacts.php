3311c38d26f5e0fa8b20b0e682d91c032e2ba338
jyotiprava <jyoti.sahoo@krititech.in>
2013-10-22 12:33:27 +0530

    Update allacts.php

--- findtheact/application/models/allacts.php
+++ findtheact/application/models/allacts.php
@@ -3,22 +3,27 @@ Class Allacts extends CI_Model
 {
 
 /*************************************** view all event manager details *******************************/
-
+//call this function by passing login user emailid
  function allactdetails($uid)
  {
+ 
+ //build a query to retrieve the act agency name under which the login act person resides
  
  $query1 = $this -> db -> get_where('profile',array('email'=>$uid));
  $row = $query1->row(); 
 
+//get the act agency name and store in a varible called agencyid
 
 $agencyid=$row->added_by;
 
+//if the agencyid is not null
+
  if($agencyid!='')
  {
-
+//built a query to get all the act agency name except under which the act person resides
    $query = $this -> db -> get_where('registration',array('type'=>'1','email !='=>$agencyid));
 
-  
+  //return the result value
      return $query->result();
  }
  
