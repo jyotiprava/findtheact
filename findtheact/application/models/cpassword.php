9343c15b0e8661a8adc74f3214c5aae71a774dc7
jyotiprava <jyoti.sahoo@krititech.in>
2013-10-22 13:20:43 +0530

    Update cpassword.php

--- findtheact/application/models/cpassword.php
+++ findtheact/application/models/cpassword.php
@@ -6,16 +6,19 @@
 	
 	 
         function cpassw($email,$pass,$cpass){
+       //built a query to get the existing password from registration table where email equal to the login act person email and type =2
 	$query = $this->db->get_where('registration',array('email'=>$email,'type'=>2));
 	$data=$query->result();
+	//get the password of the corresponding login user
 	$pwd=$data[0]->pwd;
+	//if the getting password is not equal to the post password value the through an invalid password error
 	if($pwd!=$cpass)
 	{
 	    echo "Current Password Is Not Valid";
 	}
 	else{
 	    
-	    
+	  //else built a query to update the existing password with the new password of the corresponding user  
 	     $insert = array(
                         'pwd'=>$pass
                                      
